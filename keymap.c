debf6b76fbd87adbac031ca8d24e4544e279d2cd
Luc Wastiaux <luc.wastiaux@airpost.net>
2016-07-04 23:46:23 +0800

    introduction of symbol layer

--- keymap.c
+++ keymap.c
@@ -8,7 +8,7 @@
 #define KEY_NAV 3 // key navigation layer
 #define KEY_SEL 4 // key selection layer
 #define NUMBER  5  // number layer
-#define SYMB 10 // symbols
+#define SYMBOL  6
 #define MDIA 11 // media keys
 
 // macros
@@ -26,10 +26,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // base layer
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_ESC,         KC_1,           KC_2,    KC_3,   KC_4,               KC_5,   KC_LEFT,
-        KC_TAB,         KC_QUOT,        KC_COMM, KC_DOT, KC_P,               KC_Y,   MO(KEY_SEL),
-        KC_CAPSLOCK,    KC_A,           KC_O,    KC_E,   LT(NUMBER, KC_U),   KC_I,
-        KC_LSFT,        KC_SCLN,        KC_Q,    KC_J,   KC_K,               KC_X,   MO(KEY_NAV),
+        KC_ESC,         KC_1,           KC_2,    KC_3,                KC_4,               KC_5,   KC_LEFT,
+        KC_TAB,         KC_QUOT,        KC_COMM, LT(KEY_SEL, KC_DOT), LT(KEY_NAV, KC_P),  KC_Y,   MO(KEY_SEL),
+        KC_CAPSLOCK,    KC_A,           KC_O,    LT(SYMBOL, KC_E),    LT(NUMBER, KC_U),   KC_I,
+        KC_LSFT,        KC_SCLN,        KC_Q,    KC_J,                KC_K,               KC_X,   MO(KEY_NAV),
                    TG(SHELL),KC_TRNS,KC_TRNS,KC_TRNS,MO(NUMBER),
                                               // thumb cluster
                                                        KC_LCTRL,     KC_LALT,
@@ -199,22 +199,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                 `--------------------'       `--------------------'
  */
 // SYMBOLS
-[SYMB] = KEYMAP(
+[SYMBOL] = KEYMAP(
        // left hand
-       KC_TRNS,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_TRNS,
+       KC_TRNS,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,
+       /*
        KC_TRNS,KC_EXLM,KC_AT,  KC_LCBR,KC_RCBR,KC_PIPE,KC_TRNS,
        KC_TRNS,KC_HASH,KC_DLR, KC_LPRN,KC_RPRN,KC_GRV,
        KC_TRNS,KC_PERC,KC_CIRC,KC_LBRC,KC_RBRC,KC_TILD,KC_TRNS,
+       */
+       KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
+       KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
+       KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,       
        KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
                                        KC_TRNS,KC_TRNS,
                                                KC_TRNS,
                                KC_TRNS,KC_TRNS,KC_TRNS,
        // right hand
-       KC_TRNS, KC_F6,   KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F11,
-       KC_TRNS, KC_UP,   KC_7,   KC_8,    KC_9,    KC_ASTR, KC_F12,
-                KC_DOWN, KC_4,   KC_5,    KC_6,    KC_PLUS, KC_TRNS,
-       KC_TRNS, KC_AMPR, KC_1,   KC_2,    KC_3,    KC_BSLS, KC_TRNS,
-                         KC_TRNS,KC_DOT,  KC_0,    KC_EQL,  KC_TRNS,
+       KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS,
+       KC_TRNS, KC_UP,   KC_LPRN, KC_RPRN, KC_9,    KC_ASTR, KC_TRNS,
+                KC_DOWN, KC_LBRC, KC_RBRC, KC_6,    KC_PLUS, KC_TRNS,
+       KC_TRNS, KC_AMPR, KC_LCBR, KC_RCBR, KC_3,    KC_BSLS, KC_TRNS,
+                          KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
        KC_TRNS, KC_TRNS,
        KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS
@@ -263,7 +268,9 @@ KEYMAP(
 };
 
 const uint16_t PROGMEM fn_actions[] = {
+/*
     [1] = ACTION_LAYER_TAP_TOGGLE(SYMB)                // FN1 - Momentary Layer 1 (Symbols)
+*/    
 };
 
 
