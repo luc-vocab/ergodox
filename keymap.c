d7a1237fe63991ce4560c42cd83384d23117b54e
lucwastiaux <luc.wastiaux@airpost.net>
2016-09-13 19:26:14 +0800

    added backward/forward navigation keys on shortcut layer

--- keymap.c
+++ keymap.c
@@ -233,7 +233,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                 KC_TRNS, HYPR(KC_4),    HYPR(KC_5),    HYPR(KC_6),    KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS, HYPR(KC_1),    HYPR(KC_2),    HYPR(KC_3),    KC_TRNS, KC_TRNS,
                          RCTL(KC_LBRC), RCTL(KC_RBRC),KC_TRNS,KC_TRNS,KC_TRNS,
-       KC_TRNS, KC_TRNS,
+       LALT(KC_LEFT),  LALT(KC_RIGHT),
        KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS
 ),
